24ba2939add69b06b0ef41af413a38add174886c
Dranas786 <dranas2002@gmail.com>
2025-01-17 19:27:54 -0700

    Home

--- movie-browser/src/App.jsx
+++ movie-browser/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import MovieCard from "./components/MovieCard";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <MovieCard movie={{ title: "Filmy", release_date: "2024" }} />
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
---
+++ movie-browser/src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import MovieCard from "../components/MovieCard";
+
+function Home() {
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 1, title: "Johnny", release_date: "2022" },
+  ];
+
+  const handleSearch = () => {};
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies.."
+          className="search-input"
+        />
+      </form>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
