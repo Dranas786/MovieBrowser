8dee9d0c284563ffbb33ffa29f400459750aad53
Dranas786 <dranas2002@gmail.com>
2025-01-18 21:47:51 -0700

    added search functtionality

--- movie-browser/src/pages/Home.jsx
+++ movie-browser/src/pages/Home.jsx
@@ -24,9 +24,22 @@ function Home() {
     loadPopularMovies();
   }, []);
 
-  const handleSearch = (e) => {
+  const handleSearch = async (e) => {
     e.preventDefault();
-    alert(searchQuery);
+    if (!searchQuery.trim()) return;
+    if (loadiing) return;
+    setLoading(true);
+    try {
+      const searchResults = await searchMovies(searchQuery);
+      setMovies(searchResults);
+      setError(null);
+    } catch (err) {
+      console.log(error);
+      setError("Failed to search");
+    } finally {
+      setLoading(false);
+    }
+    setSearchQuery("");
   };
 
   return (
@@ -43,14 +56,20 @@ function Home() {
           Search
         </button>
       </form>
-      <div className="movies-grid">
-        {movies.map(
-          (movie) =>
-            movie.title.toLowerCase().startsWith(searchQuery) && (
-              <MovieCard movie={movie} key={movie.id} />
-            )
-        )}
-      </div>
+
+      {error && <div className="error-message">{error}</div>}
+      {loadiing ? (
+        <div className="loading">Loading... </div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map(
+            (movie) =>
+              movie.title.toLowerCase().startsWith(searchQuery) && (
+                <MovieCard movie={movie} key={movie.id} />
+              )
+          )}
+        </div>
+      )}
     </div>
   );
 }
